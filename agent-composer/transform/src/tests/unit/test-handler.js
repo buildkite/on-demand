5500449d25c2de1bf69d4935213410659d03e096
Keith Duncan <keith_duncan@me.com>
2020-03-31 20:36:34 +1000

    Add a real test!

--- agent-composer/transform/src/tests/unit/test-handler.js
+++ agent-composer/transform/src/tests/unit/test-handler.js
@@ -3,20 +3,60 @@
 const app = require('../../app.js');
 const chai = require('chai');
 const expect = chai.expect;
-var event, context;
+var event = {
+    region: "us-east-1",
+    accountId: "$ACCOUNT_ID",
+    fragment: {
+        AWSTemplateFormatVersion: "2010-09-09",
+        Globals: {
+
+        },
+        Resources: {
+            Terraform: {
+                Type: "Buildkite::ECS::Agent",
+                Properties: {
+                    Image: "hashicorp/terraform:light",
+                    BuildkiteAgentImage: {
+                         "Fn::GetAtt": "BuildAgentSidecar.Outputs.Image"
+                    },
+                    SshAgentBackend: {
+                        "Fn::FindInMap": [
+                            "AgentConfig",
+                            {
+                                "Ref": "AWS::Region"
+                            },
+                            "SshBackend"
+                        ]
+                    },
+                    TaskFamily: "terraform"
+                }
+            }
+        }
+    },
+    transformId: "$TRANSFORM_ID",
+    params: {
+
+    },
+    requestId: "$REQUEST_ID",
+    templateParameterValues: {
+
+    },
+};
+var context;
 
 describe('Tests index', function () {
     it('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
+        const result = await app.handler(event, context)
 
         expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
+        expect(result.requestId).to.equal("$REQUEST_ID");
+        expect(result.status).to.equal("success");
+        expect(result.fragment).to.be.an('object');
+        expect(result.fragment.Resources).to.be.an('object');
 
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
+        expect(result.fragment.Resources['TerraformTaskDefinition']).to.be.an('object');
+        expect(result.fragment.Resources['TerraformLogGroup']).to.be.an('object');
+        expect(result.fragment.Resources['TerraformExecutionRole']).to.be.an('object');
+        expect(result.fragment.Resources['TerraformTaskRole']).to.be.an('object');
     });
 });
