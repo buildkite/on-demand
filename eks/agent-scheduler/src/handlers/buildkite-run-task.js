3af5c4719c50212fbbd75714985323f9e46cfdff
Keith Duncan <keith.d@buildkite.com>
2021-11-15 15:12:41 +1000

    Progress on job scheduling

--- eks/agent-scheduler/src/handlers/buildkite-run-task.js
+++ eks/agent-scheduler/src/handlers/buildkite-run-task.js
@@ -75,17 +75,56 @@ async function sleep(ms){
     });
 }
 
-async function scheduleKubernetesJobForBuildkiteJob(k8sApi, job) {
+async function scheduleKubernetesJobForBuildkiteJob(k8sApi, namespace, buildkiteJob) {
+    // https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/gen/model/v1EnvVar.ts#L19
+    const agentVar = new k8s.V1EnvVar();
+    agentVar.name = "BUILDKITE_AGENT_TOKEN"
+    agentVar.value = "";
 
+    // https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/gen/model/v1Container.ts#L27
+    const buildkiteAgentContainer = new k8s.V1Container();
+    buildkiteAgentContainer.name = "agent"
+    buildkiteAgentContainer.image = "buildkite/agent:3"
+    buildkiteAgentContainer.env = [
+        agentToken,
+    ]
+
+    // https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/gen/model/v1PodSpec.ts#L29
+    const podSpec = new k8s.V1PodSpec();
+    podSpec.containers = [
+        buildkiteAgentContainer,
+    ];
+    podSpec.restartPolicy = "Never"
+
+    const podTemplate = new k8s.V1PodTemplateSpec();
+    podTemplate.spec = podSpec;
+
+    // https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/gen/model/v1JobSpec.ts
+    const jobSpec = new k8s.V1JobSpec();
+    jobSpec.template = podTemplate;
+
+    // https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/gen/model/v1ObjectMeta.ts
+    const metadata = new k8s.V1ObjectMeta();
+    metadata.name = buildkiteJob.uuid || buildkiteJob.id
+
+    // https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/gen/model/v1Job.ts
+    const k8sJob = new k8s.V1Job();
+    k8sJob.apiVersion = 'batch/v1';
+    k8sJob.kind = 'Job';
+    k8sJob.metadata = metadata;
+    k8sJob.spec = jobSpec;
+
+    // https://kubernetes-client.github.io/javascript/classes/batchv1api.batchv1api-1.html#createnamespacedjob
+    return k8sApi.createNamespacedJob(namespace, k8sJob)
 }
 
-async function runTaskForBuildkiteJob(k8sApi, job) {
+async function runTaskForBuildkiteJob(k8sApi, namespace, job) {
     console.log(`fn=runTaskForBuildkiteJob attempt=${attempt}`);
     
     for (var attempt = 1; attempt < 6; attempt++) {
         try {
             console.log(`fn=runTaskForBuildkiteJob attempt=${attempt} at=runTask params=${JSON.stringify(taskParams)}`);
-            let result = await scheduleKubernetesJobForBuildkiteJob(k8sApi, job);
+            let result = await scheduleKubernetesJobForBuildkiteJob(k8sApi, namespace, job);
             console.log(`fn=runTaskForBuildkiteJob attempt=${attempt} at=runTask result=${JSON.stringify(result)}`);
 
             return result;
@@ -106,6 +145,7 @@ exports.handler = async (event) => {
     console.log(`fn=handler event=${JSON.stringify(event)}`);
 
     let apiServer = process.env.KUBERNETES_API_SERVER_ENDPOINT;
+    let namespace = process.env.KUBERNETES_NAMESPACE;
 
     const cluster = {
         name: 'my-server',
@@ -130,11 +170,13 @@ exports.handler = async (event) => {
         contexts: [context],
         currentContext: context.name,
     });
+
     const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+    const batchV1Api = kc.makeApiClient(k8s.BatchV1Api);
     
     let tasks = event.Records.map(record => {
         let { job } = JSON.parse(record.body);
-        return runTaskForBuildkiteJob(k8sApi, job);
+        return runTaskForBuildkiteJob(batchV1Api, namespace, job);
     });
     
     await Promise.all(tasks);
