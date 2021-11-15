b6ba96ebc37168240085f1b7f9ff296102a50898
Keith Duncan <keith.d@buildkite.com>
2021-11-15 15:24:24 +1000

    Pass the token parameter by value for now

--- eks/agent-scheduler/src/handlers/buildkite-run-task.js
+++ eks/agent-scheduler/src/handlers/buildkite-run-task.js
@@ -79,7 +79,7 @@ async function scheduleKubernetesJobForBuildkiteJob(k8sApi, namespace, buildkite
     // https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/gen/model/v1EnvVar.ts#L19
     const agentVar = new k8s.V1EnvVar();
     agentVar.name = "BUILDKITE_AGENT_TOKEN"
-    agentVar.value = "";
+    agentVar.value = process.env.BUILDKITE_AGENT_TOKEN;
 
     // https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/gen/model/v1Container.ts#L27
     const buildkiteAgentContainer = new k8s.V1Container();
