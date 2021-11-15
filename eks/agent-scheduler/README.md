a46460b9087393e5a7d71dc993d87b0de6d83603
Keith Duncan <keith.d@buildkite.com>
2021-11-15 13:27:20 +1000

    Add an eks agent-scheduler that is a copy of the ecs scheduler

---
+++ eks/agent-scheduler/README.md
@@ -0,0 +1,98 @@
+# agent-scheduler
+
+This project contains source code and supporting files for scheduling Buildkite
+Agents using AWS Elastic Container Service in response to builds.
+
+- `src` - Code for the Lambda functions.
+- `template.yml` - A CloudFormation template that defines the AWS resources.
+
+## Prerequisites
+
+This stack has the following prerequisites that must be deployed beforehand:
+
+* **Buildkite Agent Registration Token SSM Parameter**: A `String` or
+`SecretString` parameter that stores an agent registration token.
+* **EventBridge Bus**: A AWS EventBridge bus that is associated with a Buildkite
+partner event source.
+* **ECS Cluster**: An ECS Cluster that will be used to schedule tasks.
+* **VPC Subnets**: VPC subnets to schedule tasks in, must have network access
+to the Buildkite API but can otherwise be public or private subnets.
+
+The [on-demand template](../) will create these resources for you. Consider
+whether you want to deploy the on-demand template instead.
+
+## Deploy using the AWS Serverless Application Repository web console
+
+[![Deploy AWS Serverless Application](https://cdn.rawgit.com/buildkite/cloudformation-launch-stack-button-svg/master/launch-stack.svg)](https://serverlessrepo.aws.amazon.com/applications/arn:aws:serverlessrepo:us-east-1:832577133680:applications~buildkite-on-demand-scheduler)
+
+The serverless application repository console will ask you for values for the
+following parameters:
+
+* **Application Name**: Use something descriptive, the default value of
+buildkite-on-demand-scheduler is fine.
+* **Parameter BuildkiteAgentTokenParameterPath**: An AWS SSM Parameter path that
+stores a Buildkite Agent Registration token for this deployment to use. This can
+be a `String` or `SecretString` parameter and must already exist. See the
+[Buildkite Agent Tokens Documentation](https://buildkite.com/docs/agent/v3/tokens)
+for details.
+* **Parameter EventBridgeBusName**: The name of an Amazon EventBridge Bus
+associated with a Buildkite Partner Event source **NB** ensure you provide the
+name of the EventBus name _not_ the EventBus ARN.
+* **Parameter BuildkiteQueue**: The name of the Buildkite queue this stack will
+service. You will use this queue name in your Buildkite Pipeline Agent Query
+rules e.g. `queue=my-queue-name`.
+* **Parameter EcsClusterName**: The cluster name to schedule agent task
+definitions using.
+* **Parameter VpcSubnetIds**: A comma separated list of VPC subnet IDs to
+schedule agents in.
+
+When creating the stack you will need to check the option to acknowledge that
+the app creates custom IAM roles.
+
+## Deploy using the AWS Serverless Application Model command line interface
+
+The AWS SAM CLI is an extension of the AWS CLI that adds functionality for
+building and testing Lambda applications. See the Amazon documentation for help
+[installing the AWS SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html). These instructions were
+written using SAM Version 0.40.0.
+
+To deploy `agent-scheduler` for the first time, run the following in your shell:
+
+```bash
+sam deploy --capabilities CAPABILITY_IAM --guided
+```
+
+This command will package and deploy `agent-scheduler` to your AWS account, and
+present you with a series of prompts:
+
+* **Stack Name**: The name of the stack to deploy to CloudFormation. This should
+be unique to your account and region, something like
+`buildkite-on-demand-agent-scheduler`.
+* **AWS Region**: The AWS region you want to deploy `agent-scheduler` to and run
+your Buildkite builds in. `agent-scheduler` can be deployed to multiple regions
+allowing you to target specific regions using Buildkite Agent Query Rules.
+* **Parameter EventBridgeBusName**: The name of the Amazon EventBridge Bus
+associated with a Buildkite Partner Event source **NB** you provide the name of
+the EventBus name _not_ the EventBus ARN.
+* **Parameter BuildkiteQueue**: The name of the Buildkite queue this stack will
+service. You will use this queue name in your Buildkite Pipeline Agent Query
+rules e.g. `queue=my-queue-name`.
+* **Parameter BuildkiteAgentTokenParameterPath**: An AWS SSM Parameter path that
+stores a Buildkite Agent Registration token for this deployment to use. This can
+be a `String` or `SecretString` parameter and must already exist. See the
+[Buildkite Agent Tokens Documentation](https://buildkite.com/docs/agent/v3/tokens)
+for details.
+* **Parameter VpcSubnetIds**: A comma separated list of VPC subnet IDs to
+schedule agents in.
+* **Parameter EcsClusterName**: The cluster name to schedule agent task
+definitions using.
+* **Confirm changes before deploy**: If set to yes, any change sets will be
+shown to you before execution for manual review. If set to no, the AWS SAM CLI
+will automatically deploy changes.
+* **Allow SAM CLI IAM role creation**: You must answer yes to this prompt. This
+SAM application creates an AWS IAM role for your ECS task definitions and roles
+for the AWS Lambda functions. These are scoped down to minimum required
+permissions.
+* **Save arguments to samconfig.toml**: Set to yes so your choices are saved to
+a configuration file inside the project. In the future you can just re-run
+`sam deploy` without parameters to deploy changes.
---
+++ eks/agent-scheduler/src/handlers/buildkite-run-task.js
@@ -0,0 +1,398 @@
+const AWS = require('aws-sdk');
+
+function getAgentQueryRule(rule, agentQueryRules) {
+    let taskDefinition = agentQueryRules.filter(query_rule => {
+            return query_rule.startsWith(`${rule}=`);
+        })
+        .map(query_rule => {
+            return query_rule.split("=")[1];
+        })
+        .shift();
+    
+    return taskDefinition;
+}
+
+function getDefaultEcsRunTaskParams(cluster, job) {
+    let jobId = job.uuid || job.id;
+    let subnets = process.env.VPC_SUBNETS.split(",");
+
+    let vpcConfiguration;
+    let launchType = process.env.LAUNCH_TYPE;
+    switch (launchType) {
+        case "FARGATE":
+            // Scheduled in a public or private subnet with a 0.0.0.0/0 gateway
+            // route.
+            vpcConfiguration = {
+                assignPublicIp: "ENABLED",
+                subnets: subnets,
+            };
+            break;
+        case "EC2":
+            // Scheduled in a private subnet with a 0.0.0.0/0 NAT gateway route.
+            vpcConfiguration = {
+                subnets: subnets,
+            };
+            break;
+        default:
+            throw `unsupported LAUNCH_TYPE environment variable: ${launchType}`;
+            break;
+    }
+    
+    let params = {
+        cluster: cluster,
+        count: 1,
+        launchType: launchType,
+        networkConfiguration: {
+            awsvpcConfiguration: vpcConfiguration,
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: "agent",
+                    command: [
+                        "start",
+                        "--disconnect-after-job",
+                        "--disconnect-after-idle-timeout=10"
+                    ],
+                    environment: [
+                        {
+                            "name": "BUILDKITE_AGENT_ACQUIRE_JOB",
+                            "value": jobId,
+                        },
+                    ]
+                }
+            ],
+        },
+        taskDefinition: "buildkite",
+    };
+    
+    return params;
+}
+
+function range(start, stop) {
+    if (typeof stop == 'undefined') {
+        // one param defined
+        stop = start;
+        start = 0;
+    }
+
+    let step = 1024;
+
+    var result = [];
+    for (var i = start; step > 0 ? i <= stop : i > stop; i += step) {
+        result.push(i);
+    }
+
+    return result;
+}
+
+function memoryRangeForCpu(cpu) {
+    if (cpu == 256) {
+        return [512, 1024, 2048];
+    } else if (cpu == 512) {
+        return range(1024, 4096);
+    } else if (cpu == 1024) {
+        return range(2048, 8192);
+    } else if (cpu == 2048) {
+        return range(4096, 16384);
+    } else /* if cpu == 4096 */ {
+        return range(8192, 30720);
+    }
+}
+
+function cpuValues() {
+    return [256, 512, 1024, 2048, 4096]
+}
+
+function supportedCpuMemoryValues() {
+    return cpuValues()
+        .flatMap(cpu => {
+            return memoryRangeForCpu(cpu).map(memory => [cpu, memory])
+        });
+}
+
+function atLeastCpuMemory(requestedCpu, requestedMemory) {
+    let supported = supportedCpuMemoryValues();
+
+    for (let [cpu, memory] of supported) {
+        if (cpu < requestedCpu || memory < requestedMemory) {
+            continue
+        }
+
+        return [cpu, memory]
+    }
+
+    return supported[supported.length - 1];
+}
+
+async function getEcsRunTaskParamsForJob(cluster, job) {
+    let taskParams = getDefaultEcsRunTaskParams(cluster, job);
+
+    let taskDefinition = getAgentQueryRule("task-definition", job.agent_query_rules);
+    if (taskDefinition != undefined) {
+        // Task definition is overridden...
+
+        console.log(`fn=getEcsRunTaskParamsForJob taskDefinition=${taskDefinition}`);
+        taskParams.taskDefinition = taskDefinition;
+    } else {
+        let image = getAgentQueryRule("image", job.agent_query_rules);
+        if (image != undefined) {
+            let requestedCpu = parseInt(getAgentQueryRule("cpu", job.agent_query_rules) || 256);
+            let requestedMemory = parseInt(getAgentQueryRule("memory", job.agent_query_rules) || 512);
+
+            /*
+                Find the supported cpu memory combination that is at least
+                the cpu and memory requested.
+            */
+            let [cpu, memory] = atLeastCpuMemory(requestedCpu, requestedMemory);
+
+            console.log(`fn=getEcsRunTaskParamsForJob image=${image} cpu=${cpu} memory=${memory}`);
+
+            /*
+                Synthesise a task definition for this image, cpu and memory.
+                
+                1. Generate a task definition name.
+                2. Call ecs:RegisterTaskDefinition with agent sidecar (and iam-ssh-agent sidecar)
+            */
+
+            /*
+                Generate a name for this task family.
+
+                Up to 255 letters (uppercase and lowercase), numbers, and
+                hyphens are allowed.
+
+                Image could be:
+                012345678910.dkr.ecr.us-east-1.amazonaws.com/agent/buildkite:latest
+                quay.io/organization/image
+                keithduncan/agent@sha256:94afd1f2e64d908bc90dbca0035a5b567EXAMPLE
+            */
+
+            /*
+                Sticking the year and month in the name gives us 1,000,000 per
+                month or ~32,000 builds per image tag per day. More than enough
+                for anyone!
+
+                If anyone needs more significant bits we can add the day.
+            */
+            let date = new Date;
+            let year = (new Intl.DateTimeFormat('en-US-POSIX', { year: 'numeric' })).format(date);
+            let month = (new Intl.DateTimeFormat('en-US-POSIX', { month: '2-digit' })).format(date);
+
+            let taskFamily = `ondemand-${year}${month}-${image.replace(/[^a-zA-Z0-9]/gi, '')}`.substring(0, 255);
+
+            let logConfiguration = {
+                logDriver: "awslogs",
+                options: {
+                    "awslogs-region": process.env.AWS_REGION,
+                    // Log group names can be up to 512 characters,
+                    // including / a-z A-Z.
+                    "awslogs-group": `/aws/ecs/${taskFamily}`,
+                    "awslogs-stream-prefix": "ecs",
+                    "awslogs-create-group": "true",
+                }
+            };
+
+            let params = {
+                family: taskFamily,
+                executionRoleArn: process.env.DEFAULT_EXECUTION_ROLE_ARN,
+                networkMode: "awsvpc",
+                cpu: `${cpu}`,
+                memory: `${memory}`,
+                containerDefinitions: [
+                    {
+                        name: "agent-init",
+                        image: "keithduncan/buildkite-sidecar",
+                        essential: false,
+                        entryPoint: [
+                            '/bin/sh',
+                            '-c',
+                        ],
+                        command: [
+                            'echo container=agent-init at=initalised',
+                        ],
+                        logConfiguration: logConfiguration,
+                    },
+                ],
+                volumes: [],
+                requiresCompatibilities: [
+                    "FARGATE",
+                ],
+            }
+
+            let iamSshAgentBackendUrl = process.env.IAM_SSH_AGENT_BACKEND_URL;
+            let includeSshAgent = (iamSshAgentBackendUrl != undefined);
+
+            if (includeSshAgent) {
+                params.containerDefinitions.push({
+                    name: "ssh-agent",
+                    image: "keithduncan/iam-ssh-agent:0.2",
+                    essential: true,
+                    command: [
+                        'iam-ssh-agent',
+                        'daemon',
+                        '--bind-to=/ssh/socket',
+                    ],
+                    logConfiguration: logConfiguration,
+                    environment: [
+                        {
+                            name: "IAM_SSH_AGENT_BACKEND_URL",
+                            value: iamSshAgentBackendUrl,
+                        }
+                    ],
+                    healthCheck: {
+                        command: [
+                            'test',
+                            '-S',
+                            '/ssh/socket',
+                        ],
+                    },
+                    mountPoints: [
+                        {
+                            sourceVolume: "ssh-agent",
+                            containerPath: "/ssh",
+                        }
+                    ],
+                });
+
+                params.volumes.push({
+                    name: "ssh-agent",
+                });
+            }
+
+            let agentContainer = {
+                name: "agent",
+                image: image,
+                essential: true,
+                entryPoint: [
+                    "/buildkite/bin/buildkite-agent"
+                ],
+                command: [
+                    "start",
+                ],
+                environment: [
+                    {
+                        name: "BUILDKITE_BUILD_PATH",
+                        value: "/buildkite/builds",
+                    },
+                    {
+                        name: "BUILDKITE_HOOKS_PATH",
+                        value: "/buildkite/hooks",
+                    },
+                    {
+                        name: "BUILDKITE_PLUGINS_PATH",
+                        value: "/buildkite/plugins",
+                    },
+                ],
+                secrets: [
+                    {
+                        name: "BUILDKITE_AGENT_TOKEN",
+                        valueFrom: process.env.BUILDKITE_AGENT_TOKEN_PARAMETER_PATH,
+                    }
+                ],
+                volumesFrom: [
+                    {
+                        sourceContainer: "agent-init",
+                    }
+                ],
+                dependsOn: [
+                    {
+                        containerName: "agent-init",
+                        condition: "SUCCESS",
+                    }
+                ],
+                logConfiguration: logConfiguration,
+                mountPoints: [],
+            };
+            if (includeSshAgent) {
+                agentContainer.environment.push({
+                    name: "SSH_AUTH_SOCK",
+                    value: "/ssh/socket"
+                });
+                agentContainer.mountPoints.push({
+                    sourceVolume: "ssh-agent",
+                    containerPath: "/ssh",
+                });
+                agentContainer.dependsOn.push({
+                    containerName: "ssh-agent",
+                    condition: "HEALTHY",
+                });
+            }
+            params.containerDefinitions.push(agentContainer);
+
+            /*
+                AWS will store up to 1,000,000 task definition revisions.
+
+                https://docs.aws.amazon.com/general/latest/gr/ecs-service.html#limits_ecs
+
+                The docs say unregistering does nothing for this limit.
+
+                To escape the limit the naming scheme needs a random element.
+            */
+            let ecs = new AWS.ECS({apiVersion: '2014-11-13'});
+            let result = await ecs.registerTaskDefinition(params).promise();
+
+            let taskDefinition = `${result.taskDefinition.family}:${result.taskDefinition.revision}`;
+            console.log(`fn=getEcsRunTaskParamsForJob image=${image} taskDefinition=${taskDefinition}`);
+            taskParams.taskDefinition = taskDefinition;
+        }
+    }
+
+    let taskRole = getAgentQueryRule("task-role", job.agent_query_rules);
+    if (taskRole != undefined) {
+        let taskRoleArn = `${process.env.TASK_ROLE_ARN_PREFIX}${taskRole}`;
+
+        console.log(`fn=getEcsRunTaskParamsForJob taskRoleArn=${taskRoleArn}`);
+        taskParams.overrides.taskRoleArn = taskRoleArn;
+    }
+    
+    return taskParams;
+}
+
+async function sleep(ms){
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    });
+}
+
+async function runTaskForBuildkiteJob(cluster, job) {
+    console.log(`fn=runTaskForBuildkiteJob attempt=${attempt}`);
+    
+    for (var attempt = 1; attempt < 6; attempt++) {
+        try {
+            let ecs = new AWS.ECS({apiVersion: '2014-11-13'});
+            let taskParams = await getEcsRunTaskParamsForJob(cluster, job);
+
+            console.log(`fn=runTaskForBuildkiteJob attempt=${attempt} at=runTask params=${JSON.stringify(taskParams)}`);
+            let result = await ecs.runTask(taskParams).promise();
+            console.log(`fn=runTaskForBuildkiteJob attempt=${attempt} at=runTask result=${JSON.stringify(result)}`);
+
+            return result;
+        }
+        catch (e) {
+            console.log(`fn=runTaskForBuildkiteJob attempt=${attempt} at=error error=${e}`);
+            
+            await sleep(1000 * Math.pow(attempt, 2));
+            
+            continue;
+        }
+    }
+    
+    throw new Error("Couldn't start ECS task after 5 attempts");
+}
+
+exports.handler = async (event) => {
+    console.log(`fn=handler event=${JSON.stringify(event)}`);
+
+    let cluster = process.env.ECS_CLUSTER_NAME;
+    
+    let tasks = event.Records.map(record => {
+        let { job } = JSON.parse(record.body);
+        return runTaskForBuildkiteJob(cluster, job);
+    });
+    
+    await Promise.all(tasks);
+
+    return {
+        statusCode: 200,
+    };
+};
